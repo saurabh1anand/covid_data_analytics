abdab339ee69dd40e165cffa70b388997b980fff
saurabh1anand <69003633+saurabh1anand@users.noreply.github.com>
2022-08-15 15:39:16 +0530

    Covid Data Analytics Project - July 2022

--- covid_data_analytics V2.sql
+++ covid_data_analytics V2.sql
@@ -128,6 +128,11 @@ from #PercentagePeopleVaccinated
 
 
 
+
+
+
+
+
 -- creating view to store data for later visualization 
 create view PercentagePeopleVaccinated as 
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
@@ -142,4 +147,65 @@ where dea.continent is not null
 
 select * from 
 PercentagePeopleVaccinated
+
+
+
+
+
+
+/*
+
+Quries for Visualization 
+
+
+*/
+
+
+--1. 
+
+select sum(new_cases) as total_cases, sum(cast(new_deaths as bigint)) as total_deaths , 
+		sum(cast(new_deaths as bigint))/sum(new_cases) * 100 as death_percentage
+		from portfolioProject..CovidDeaths
+		where continent is not null 
+order by 1,2 
+
+
+-- 2. 
+
+-- We take these out as they are not inluded in the above queries and want to stay consistent
+-- European Union is part of Europe
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Where continent is null 
+and location not in ('World', 'European Union', 'International', 'Upper middle income', 'high income', 'lower middle income', 'low income')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3.
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+
+-- 4.
+
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Group by Location, Population, date
+order by PercentPopulationInfected desc
+
+
+
+
+
+
 
